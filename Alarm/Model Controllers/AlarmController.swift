2ac02393d240fbd5e2a709edff97839bade15bc3
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-01-29 15:05:25 -0700

    Finished importing existing files for alarm model and datepicker. Also completed initial setup of AlarmController file

---
+++ Alarm/Model Controllers/AlarmController.swift
@@ -0,0 +1,9 @@
+//
+//  AlarmController.swift
+//  Alarm
+//
+//  Created by Taylor Bills on 1/29/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import Foundation
---
+++ Alarm/View Controllers/SwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by Taylor Bills on 1/29/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
